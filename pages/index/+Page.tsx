b26e05ddf2e340711b885eb56c4287e79d3e6eae
yuki-js <me@aoki.app>
2026-03-24 17:28:16 +0900

    delete the fucking untypographed file

--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -1,10 +1,6 @@
 import { Header } from "../../components/Header/Header";
 import { Hero } from "../../components/Hero/Hero";
 import { HomeExperienceSection } from "../../components/HomeExperienceSection/HomeExperienceSection";
-import { FeaturesSection } from "../../components/FeaturesSection/FeaturesSection";
-import { HowItWorksSection } from "../../components/HowItWorksSection/HowItWorksSection";
-import { PricingSection } from "../../components/PricingSection/PricingSection";
-import { WaitlistSection } from "../../components/WaitlistSection/WaitlistSection";
 import { Footer } from "../../components/Footer/Footer";
 
 export default function Page() {
@@ -13,8 +9,6 @@ export default function Page() {
       <Header />
       <Hero />
       <HomeExperienceSection />
-      <PricingSection />
-      <WaitlistSection />
       <Footer />
     </>
   );
